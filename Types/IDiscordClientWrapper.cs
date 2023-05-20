3aee98c9da53a5ae3e3798cbadb304fb5e3fe8fc
Alexander <satan1c@ukr.net>
2023-05-20 13:50:09 +0300

    remove unused

--- Types/IDiscordClientWrapper.cs
+++
@@ -1,9 +0,0 @@
-using DSharpPlus;
-
-namespace SDC_Sharp.DSharpPlus.Types
-{
-    public interface IDiscordClientWrapper : SDC_Sharp.Types.IDiscordClientWrapper
-    {
-        internal DiscordClient Client { get; }
-    }
-}
